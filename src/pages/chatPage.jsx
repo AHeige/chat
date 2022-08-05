12c504c758be126bde04e6b4e8bcb46166fca5bb
AHeige <aheige17@gmail.com>
2022-08-05 07:50:57 +0200

    Chat message input styling

--- src/pages/chatPage.jsx
+++ src/pages/chatPage.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 
 import Box from "@mui/material/Box"
-import TextField from "@mui/material/TextField"
-import AppBar from "@mui/material/AppBar"
+import BottomNavigation from "@mui/material/BottomNavigation"
 import Input from "@mui/material/Input"
+import Paper from "@mui/material/Paper"
+import SendIcon from "@mui/icons-material/Send"
 
 const ChatPage = () => {
   const sendMessage = (e) => {
@@ -13,11 +14,33 @@ const ChatPage = () => {
   }
 
   return (
-    <AppBar>
-      <Box>
-        <Input onKeyDown={(e) => sendMessage(e)}></Input>
-      </Box>
-    </AppBar>
+    <Paper
+      elevation={3}
+      style={{ position: "fixed", bottom: 0, width: "100%" }}
+    >
+      <BottomNavigation>
+        <Box
+          style={{
+            width: "50%",
+            backgroundColor: "#D3D3D3",
+            borderRadius: "25px",
+          }}
+        >
+          <Input
+            style={{
+              //backgroundColor: "#D3D3D3",
+              borderRadius: "5px",
+              width: "100%",
+              height: "100%",
+              paddingLeft: "1em",
+            }}
+            onKeyDown={(e) => sendMessage(e)}
+            placeholder="Aa"
+            disableUnderline="true"
+          ></Input>
+        </Box>
+      </BottomNavigation>
+    </Paper>
   )
 }
 
