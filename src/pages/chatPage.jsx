6c3d2bfc5203d939973577c9a2423354903626e0
Aheige <aheige17@gmail.com>
2022-08-08 00:58:31 +0200

    Chat Page showing server side messages

--- src/pages/chatPage.jsx
+++ src/pages/chatPage.jsx
@@ -1,82 +1,106 @@
-import React from "react"
+import React, { useRef, useState, useEffect } from "react"
 
 import Box from "@mui/material/Box"
 import BottomNavigation from "@mui/material/BottomNavigation"
 import Input from "@mui/material/Input"
 import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
+import Chip from "@mui/material/Chip"
 import SendIcon from "@mui/icons-material/Send"
 import Game2D from "./Game2D"
 
-const socket = new WebSocket('ws://localhost:8080');
-
-function ssend(msg) {
-    console.log("Attempting to send: " + msg)
-    if (socket || socket.readyState === 1) {
-        socket.send(msg)
-    } else {
-        console.error("Socket not initialized, readyState=" + socket.readyState)
-    }
-}
-
-function initws() {
-    // Create WebSocket connection.
-    // Error handler
-    socket.addEventListener('error', function (event) {
-        console.error ("ws error:")
-        console.error (event)
-    });
-
-    // Connection opened
-    socket.addEventListener('open', function (event) {
-        socket.send('Hello server!');
-    });
-    
-    // Listen for messages
-    socket.addEventListener('message', function (event) {
-        console.log('Message rx: ', event.data);
-    });
-    console.log ("init ws done")
-}
-
-initws()
+//Service
 
 const ChatPage = () => {
+  const [messages, setMessages] = useState([])
+  const [inputMessage, setInputMessage] = useState("")
+  const refs = useRef(null)
+
+  const socket = new WebSocket("ws://localhost:8080")
+
+  const ssend = (msg, type) => {
+    const messageObject = {
+      message: msg,
+      messageType: type,
+    }
+
+    const x = JSON.stringify(messageObject)
+
+    console.log("Attempting to send: " + msg)
+    if (socket || socket.readyState === 1) {
+      socket.send(x)
+    } else {
+      console.error("Socket not initialized, readyState=" + socket.readyState)
+    }
+  }
+
+  // Create WebSocket connection.
+  // Error handler
+  socket.addEventListener("error", function (event) {
+    console.error("ws error:")
+    console.error(event)
+  })
+
+  // Connection opened
+  socket.addEventListener("open", function (event) {
+    //socket.send("Hello server!")
+  })
+
+  // Listen for messages
+  socket.addEventListener("message", function (event) {
+    const parsedObject = JSON.parse(event.data)
+    if (parsedObject.messageType === 1) {
+      console.log("Message rx: ", parsedObject.message)
+      setMessages((oldArray) => [...oldArray, parsedObject.message])
+    }
+  })
+  console.log("init ws done")
+
   const sendMessage = (e) => {
     if (e.key === "Enter") {
-        ssend(e.target.value)
+      const message = e.target.value
+      ssend(message, 1)
+      refs.current.value = ""
     }
   }
 
   return (
     <div>
-    <Game2D id="aster" senderFunc={ssend}></Game2D>
-    <Paper
+      <Game2D id='aster' senderFunc={ssend}></Game2D>
+      <Grid container direction={"column"} style={{ alignItems: "flex-end" }}>
+        {messages &&
+          messages.map((a, index) => (
+            <Grid item style={{ marginTop: "0.3em" }}>
+              <Chip key={index} label={a} />
+            </Grid>
+          ))}
+      </Grid>
+
+      <Paper
         elevation={3}
-        style={{ position: "fixed", bottom: 0, width: "100%" }}
-    >
-      <BottomNavigation>
-        <Box
-          style={{
-            width: "50%",
-            backgroundColor: "#D3D3D3",
-            borderRadius: "25px",
-          }}
-        >
-          <Input
+        style={{ position: "fixed", bottom: 0, width: "100%" }}>
+        <BottomNavigation>
+          <Box
             style={{
-              //backgroundColor: "#D3D3D3",
-              borderRadius: "5px",
-              width: "100%",
-              height: "100%",
-              paddingLeft: "1em",
-            }}
-            onKeyDown={(e) => sendMessage(e)}
-            placeholder="Aa"
-            disableUnderline="true"
-          ></Input>
-        </Box>
-      </BottomNavigation>
-    </Paper>
+              width: "50%",
+              backgroundColor: "#D3D3D3",
+              borderRadius: "25px",
+            }}>
+            <Input
+              style={{
+                //backgroundColor: "#D3D3D3",
+                borderRadius: "5px",
+                width: "100%",
+                height: "100%",
+                paddingLeft: "1em",
+              }}
+              onKeyDown={(e) => sendMessage(e)}
+              placeholder='Aa'
+              disableUnderline={true}
+              inputRef={refs}></Input>
+          </Box>
+        </BottomNavigation>
+      </Paper>
     </div>
   )
 }
--- src/server.js
+++ src/server.js
@@ -1,21 +1,23 @@
-const ws = require('ws')
+const ws = require("ws")
 const PORT = 8080
-const wss = new ws.WebSocketServer({ port: PORT });
+const wss = new ws.WebSocketServer({ port: PORT })
 
 // some connected user did send a message. DateTime, UserID, message.
 // clients are a user with a UserID. OnMessageSent the client send a ws packet with messageData
 
-function init () {
-    wss.on('connection', function connection(ws) {
-        ws.on('message', function message(data) {
-            console.log('received: %s', data);
-            ws.send("Got: " + data);
-        });
-        const msg = 'Hello client!'
-        ws.send(msg);
-        console.log("send: " + msg)
-    });
-    console.log("server initialized")
+function init() {
+  wss.on("connection", function connection(ws) {
+    ws.on("message", function message(data) {
+      let parsedObject = JSON.parse(data)
+      let stringyfiedObject = JSON.stringify(parsedObject)
+      console.log("received: %s", data)
+      ws.send(stringyfiedObject)
+    })
+    const msg = { message: "Hello client!", messageType: 2 }
+    ws.send(JSON.stringify(msg))
+    console.log("send: " + msg.message)
+  })
+  console.log("server initialized")
 }
 
 init()
